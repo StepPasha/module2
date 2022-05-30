97ee103e4d14b66be91ef95bc43515c6b5206289
StepPasha <pphudc@inbox.ru>
2022-05-30 18:42:43 +0300

    Homework2

--- src/main/java/ru/gb/stepanov/hm2/MyArrayDataException.java
+++ src/main/java/ru/gb/stepanov/hm2/MyArrayDataException.java
@@ -1,8 +1,8 @@
 package ru.gb.stepanov.hm2;
 
 public class MyArrayDataException extends Exception{
-    public int i;
-    public int j;
+    private int i;
+    private int j;
 
     public MyArrayDataException(int i, int j) {
 
